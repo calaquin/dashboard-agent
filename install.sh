bedad795b3e115c21faf191cfff2346d074d3426
calaquin <calaquin@gmail.com>
2026-09-13 16:55:22 -0400

    Bump version to 0.3.15 in installer script

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ UNINSTALL=0
 REENROLL=0
 REMOVE_OLD=0
 FORCE=0
-TAG="${DASHBOARD_AGENT_TAG:-v0.3.13}"
+TAG="${DASHBOARD_AGENT_TAG:-v0.3.15}"
 REPO_RAW_URL="https://raw.githubusercontent.com/calaquin/dashboard-agent/${TAG}"
 
 prompt_yn() {
