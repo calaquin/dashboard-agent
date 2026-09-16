5e2cbe001e5978d2cf17f14ab958056a4b8a96a9
calaquin <calaquin@gmail.com>
2026-09-16 12:35:29 -0400

    bump: update AGENT_VERSION and install script tag to 0.7.2

--- install.sh
+++ install.sh
@@ -18,7 +18,6 @@ UNINSTALL=0
 REENROLL=0
 REMOVE_OLD=0
 FORCE=0
-TAG="${DASHBOARD_AGENT_TAG:-v0.7.1}"
 TAG="${DASHBOARD_AGENT_TAG:-v0.7.2}"
 REPO_RAW_URL="https://raw.githubusercontent.com/calaquin/dashboard-agent/${TAG}"
 
